259d28fa8ee02aec9ade82e4e8d20c56087e61ac
bishalhota <bishalhota264@gmail.com>
2025-06-01 22:39:20 +0530

    myorderspage

--- Frontend/src/Pages/MyOrdersPage.jsx
+++ Frontend/src/Pages/MyOrdersPage.jsx
@@ -24,7 +24,7 @@ const MyOrdersPage = () => {
                 {
                     _id:"345678",
                     createdAt: new Date(),
-                    shippingAddress:{city:"New York",country:"USA"},
+                    shippingAddress:{city:"New York", country:"USA"},
                     OrderItems:[
                         {
                             name:"Product 2",
@@ -35,10 +35,12 @@ const MyOrdersPage = () => {
                     isPaid:true,
                 },
             ];
-
+            
             setOrders(mockOrders);;
         },1000);
     },[]);
+
+
 
   return (
     <div className='max-w-7xl mx-auto p-4 sm:p-6'>
@@ -56,6 +58,34 @@ const MyOrdersPage = () => {
                         <th className='py-2 px-4 sm:py-3'>Status</th>
                     </tr>
                 </thead>
+                <tbody>{orders.length > 0 ?(
+                    orders.map((order) =>(
+                        <tr key={order._id} className='border-b hover:border-gray-50 cursor-pointer'>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                                <img src={order.OrderItems[0].image} alt="" className='w-10 h-10 sm:w-12 sm:h-12' object-cover rounded-lg />
+                            </td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap '>#{order._id}</td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                                {new Date(order.createdAt).toLocaleDateString()}{" "}
+                                {new Date(order.createdAt).toLocaleTimeString()}
+                            </td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                                {order.shippingAddress ? `${order.shippingAddress.city}`:"N/A"}
+                            </td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>{order.orderItems.length}</td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>Rs{order.totalPrice}</td>
+                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                                <span className={`${order.isPaid ? "bg-green-100 text-green-700":"bg-red-100 text-red-700"}`}
+                                px-2 py-1 rounded-full text-xs sm:text-sm font-medium
+                                >{order.isPaid ? "Paid" : "Pending"}</span>
+                            </td>
+                        </tr>
+                    ))
+                ):(
+                    <tr>
+                        <td colSpan={7} className='py-4 px-4 text-center text-gray-500'>You have no orders</td>
+                    </tr>
+                )}</tbody>
             </table>
         </div>
       
