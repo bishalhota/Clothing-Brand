2e1facf065aa248b28af6fca2c3fdbbd1c8a6abf
bishalhota <bishalhota264@gmail.com>
2025-06-24 10:54:34 +0530

    Product Route done

---
+++ Backend/seeder.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("./models/Product");
+const User = require("./models/User");
+const products = require("./data/products");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+
+// seeding data
+const seedData = async () => {
+    try {
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        //create a default admin user
+        const createdUser = await User.create({
+            name:"Admin User",
+            email:"admin@admin.com",
+            password:"12334567",
+            role:"admin",
+        })
+
+        const userID = createdUser._id;
+
+        const sampleProducts = products.map((product) => {
+            return { ...product, user: userID };
+        });
+
+        await Product.insertMany(sampleProducts);
+        console.log("Data seeded successfully");
+        process.exit();
+
+    } catch (error) {
+        console.error("Error seeding data:", error);
+        process.exit(1);
+    }
+}
+
+seedData();
