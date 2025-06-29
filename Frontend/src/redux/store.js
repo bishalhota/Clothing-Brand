660e0b66d390ce68517d808b82a7130e39895513
bishalhota <bishalhota264@gmail.com>
2025-06-29 12:50:49 +0530

    redux

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -2,12 +2,18 @@ import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/ProductSlice";
 import cartReducer from "./slices/cartslice";
+import checkoutReducer from "./slices/checkoutSlice";
+import orderReducer from "./slices/orderSlice";
+import adminReducer from "./slices/adminSlice";
 
 const store = configureStore({
     reducer:{
         auth:authReducer,
         products:productReducer,
         cart:cartReducer,
+        checkout:checkoutReducer,
+        order:orderReducer,
+        admin:adminReducer,
     },
 });
 
