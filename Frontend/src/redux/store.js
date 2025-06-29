e13b36366b57785a69f6098c243e2c184cb844e9
bishalhota <bishalhota264@gmail.com>
2025-06-29 07:40:59 +0530

    cartSlice completed

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/ProductSlice";
+import cartReducer from "./slices/cartslice";
 
 const store = configureStore({
     reducer:{
         auth:authReducer,
-        products:productReducer
+        products:productReducer,
+        cart:cartReducer,
     },
 });
 
